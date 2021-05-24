a51ff831ed44dc3a8b9feeace2ad68f11d3f0a78
jpopa-po <82804778+jpopa-po@users.noreply.github.com>
2021-05-24 23:49:57 +0200

    Update ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -1,21 +1,21 @@
 #include"libft.h"
 int	ft_atoi(const char *nptr)
 {
-	int	n;
+	long	n;
 	int	i;
 	int	sign;
 
 	n = 0;
 	i = 0;
 	sign = 1;
-	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == ' ')
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
 		i++;
-	if (nptr[i] == '-')
+	if (nptr[i] == 45)
 	{
 		sign = -1;
 		i++;
 	}
-	else if (nptr[i] == '+')
+	else if (nptr[i] == 43)
 		i++;
 	while (nptr[i] && ft_isdigit(nptr[i]) == 1)
 		n = n * 10 + nptr[i++] - 48;
