d59dc7599890ab8bf639a1fa2dc7ea663f9f909b
jpopa-po <82804778+jpopa-po@users.noreply.github.com>
2021-05-25 13:35:20 +0200

    Update ft_strtrim.c

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -12,32 +12,4 @@
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s)
-{
-	char		*str;
-	int		i;
-	int		j;
-	int		len;
 
-	i = 0;
-	j = 0;
-	len = ft_strlen(s) - 1;
-	str = ft_substr(s, i, len - j - i + 1);
-	if (!str)
-		return (NULL);
-	while (s[i] == 32 || s[i] == 10 || s[i] == 9)
-		i++;
-	while (s[len - j] == 32 || s[len - j] == 10 || s[len - j] == 9)
-		j++;
-	if (i == 0 && j == 0)
-	{
-		str = ft_strdup(s);
-		return (str);
-	}
-	if (i == len + 1)
-	{
-		str = "";
-		return (str);
-	}
-	return (str);
-}
